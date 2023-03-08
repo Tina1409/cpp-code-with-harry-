c17fb724e59946350c9e8140f0e49d9129b92621
Tina <tinav1409@gmail.com>
2023-03-09 00:30:35 +0530

    typeCasting

---
+++ typeCasting.cpp
@@ -0,0 +1,11 @@
+//type casting
+#include<iostream>
+using namespace std;
+int main(){
+	float b= 6.98;
+	
+	cout<<"The value of b is "<<b<<endl;
+	cout<<"The value of b after type casting is: "<<int(b)<<endl;
+	cout<<"The value of b after type casting is: "<<(int)b<<endl;
+	
+}
