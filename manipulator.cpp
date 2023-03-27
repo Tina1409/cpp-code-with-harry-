ead6593166251c43531e02548ed26014833c0305
Tina <tinav1409@gmail.com>
2023-03-27 19:43:11 +0530

    manipulator

---
+++ manipulator.cpp
@@ -0,0 +1,14 @@
+//MANIPULATORS
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+	int a=5,b=10;
+	cout<<"The value of a withour setw is:"<<a<<endl;
+	cout<<"The value of b withour setw is:"<<b<<endl;
+	
+	cout<<"The value of a with setw is:"<<setw(3)<<a<<endl;
+	cout<<"The value of b with setw is:"<<setw(3)<<b<<endl;
+	
+	
+}
