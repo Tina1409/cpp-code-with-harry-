f78beab89f3a2578d2a9df2ccb79b78bc10c0d4d
Tina <tinav1409@gmail.com>
2023-04-07 21:50:50 +0530

    multipleInhertiance

---
+++ multipleInhertiance.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class Base1{
+	protected:
+		int base1int;
+	public:
+		void set_base1int(int a){
+			base1int =a;
+		}
+};
+
+class Base2{
+	protected:
+		int base2int;
+	public:
+		void set_base2int(int a){
+			base2int =a;
+		}
+};
+
+class Derived : public Base1,public Base2{
+	public:
+		void show(){
+			cout<<"The value of Base1 is "<<base1int<<endl;
+			cout<<"The value of Base2 is "<<base2int<<endl;
+			cout<<"The sum of these values is"<<base1int+base2int<<endl;
+		}
+};
+
+int main(){
+	Derived tina;
+	tina.set_base1int(23);
+	tina.set_base2int(25);
+	tina.show();
+
+	return 0;
+}
