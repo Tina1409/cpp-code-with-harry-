12689989585c5e517d059d090a6aecee0728e001
Tina <tinav1409@gmail.com>
2023-04-14 15:21:36 +0530

    readingAndWritingOfFiles

---
+++ readingAndWritingOfFiles.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+	/*
+	//--------------------------WRITING IN A FILE  , FLOW--> PROGRAM TO FILE
+	//connecting out file with hout stream
+	ofstream tout("tina.txt");
+	
+	//creating a name string and filling it with the string entered by the user
+	string name;
+	cout<<"Enter your name: ";
+	cin>>name;
+	
+	//writing a string to the file
+	tout<<"My name is "<< name;
+	tout.close();
+	*/
+	
+	
+	/*
+	//------------------------READING FROM A FILE
+	ifstream tin("tina.txt");
+	string content;
+	tin>>content;
+	cout<<"The content of this file is: "<<content;
+	tin.close();
+	
+	*/
+	return 0;
+}
