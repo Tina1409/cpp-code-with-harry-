1a487e4ee5b411567c56c991ddc20b9ead640ac3
Tina <tinav1409@gmail.com>
2023-05-20 22:26:05 +0530

    inlineFunctions

---
+++ inlineFunctions.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+inline int product(int a, int b){
+	return a*b;
+}
+
+int main(){
+	int a,b;
+	cout<<"Enter the value of a and b: "<<endl;
+	cin>>a>>b;
+	cout<<"The product of a and b is "<<product(a,b)<<endl;
+	cout<<"The product of a and b is "<<product(a,b)<<endl;
+	cout<<"The product of a and b is "<<product(a,b)<<endl;
+	cout<<"The product of a and b is "<<product(a,b)<<endl;
+	cout<<"The product of a and b is "<<product(a,b)<<endl;
+	cout<<"The product of a and b is "<<product(a,b)<<endl;
+	cout<<"The product of a and b is "<<product(a,b)<<endl;
+	cout<<"The product of a and b is "<<product(a,b)<<endl;
+	cout<<"The product of a and b is "<<product(a,b)<<endl;
+	cout<<"The product of a and b is "<<product(a,b)<<endl;
+	return 0;
+}
