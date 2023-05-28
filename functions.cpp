f5ffb0a64ca9f3c5b135a9ee0b6006e8c2231379
Tina <tinav1409@gmail.com>
2023-05-28 23:19:35 +0530

    functions

---
+++ functions.cpp
@@ -0,0 +1,17 @@
+//FUNCTIONS
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b){
+	int c=a+b;
+	return c;
+}
+int main(){
+	int num1,num2;
+	cout<<"Enter first number"<<endl;
+	cin>>num1;
+	cout<<"Enter second number"<<endl;
+	cin>>num2;
+	cout<<"The sum is "<<sum(num1,num2);
+	return 0;
+}
